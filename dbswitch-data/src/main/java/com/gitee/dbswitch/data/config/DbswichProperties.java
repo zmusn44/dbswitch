887b312bdb57829bac486a849b0600116fbd2321
inrgihc <inrgihc@126.com>
2020-12-20 21:40:53 +0800

    代码调试修正

--- dbswitch-data/src/main/java/com/gitee/dbswitch/data/config/DbswichProperties.java
+++ dbswitch-data/src/main/java/com/gitee/dbswitch/data/config/DbswichProperties.java
@@ -12,8 +12,10 @@ package com.gitee.dbswitch.data.config;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Properties属性映射配置
@@ -21,39 +23,42 @@ import lombok.Data;
  * @author tang
  *
  */
+@Configuration
 @Data
-@ConfigurationProperties("dbswitch")
+@ConfigurationProperties(prefix = "dbswitch", ignoreInvalidFields=false, ignoreUnknownFields = false)
 @PropertySource("classpath:config.properties")
 public class DbswichProperties {
 
-	private final List<SourceDataSourceProperties> source = new ArrayList<>();
+	private List<SourceDataSourceProperties> source = new ArrayList<>();
 
-	private final TargetDataSourceProperties target = new TargetDataSourceProperties();
+	private TargetDataSourceProperties target = new TargetDataSourceProperties();
 
 	@Data
+	@NoArgsConstructor
 	public static class SourceDataSourceProperties {
 		private String url;
 		private String driverClassName;
 		private String username;
 		private String password;
 
-		private Integer fetchSize;
-		private String sourceSchema;
-		private String sourceIncludes;
-		private String sourceExcludes;
+		private Integer fetchSize=5000;
+		private String sourceSchema="";
+		private String sourceIncludes="";
+		private String sourceExcludes="";
 	}
 
 	@Data
+	@NoArgsConstructor
 	public static class TargetDataSourceProperties {
 		private String url;
 		private String driverClassName;
 		private String username;
 		private String password;
 
-		private String targetSchema;
-		private Boolean targetDrop;
-		private Boolean createTableAutoIncrement;
-		private Boolean writerEngineInsert;
-		private Boolean changeDataSynch;
+		private String targetSchema="";
+		private Boolean targetDrop=Boolean.TRUE;
+		private Boolean createTableAutoIncrement=Boolean.FALSE;
+		private Boolean writerEngineInsert=Boolean.FALSE;
+		private Boolean changeDataSynch=Boolean.FALSE;
 	}
 }
