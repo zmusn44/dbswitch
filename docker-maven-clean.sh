0f2311aaa29622c28572bcf6852d2f2f556fd031
inrgihc <inrgihc@126.com>
2021-03-09 19:04:20 +0800

    update script

--- docker-maven-clean.sh
+++ docker-maven-clean.sh
@@ -2,8 +2,8 @@
 
 docker run -it --rm \
 	--name my-maven-project \
-	-v ~/.m2:/root/.m2 \
+	-v ~/.m2:/opt/maven/localRepository \
 	-v "$PWD":/usr/src/mymaven \
 	-w /usr/src/mymaven \
- inrgihc/aliyun-maven:3.5.0-jdk-8-alpine mvn clean
+  inrgihc/maven-aliyun:3.6.3-jdk-8 mvn clean
 
