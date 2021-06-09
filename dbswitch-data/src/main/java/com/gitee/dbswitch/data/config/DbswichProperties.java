f20b1cbf17a510bc9d9f8351dd028d09e6a9556b
tang <tang@126.com>
2021-06-09 22:42:55 +0800

    代码优化调整

--- dbswitch-data/src/main/java/com/gitee/dbswitch/data/config/DbswichProperties.java
+++ dbswitch-data/src/main/java/com/gitee/dbswitch/data/config/DbswichProperties.java
@@ -11,58 +11,54 @@ package com.gitee.dbswitch.data.config;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
- * Properties属性映射配置
- * 
- * @author tang
+ * 属性映射配置
  *
+ * @author tang
  */
 @Configuration
 @Data
-@ToString
-@ConfigurationProperties(prefix = "dbswitch", ignoreInvalidFields=false, ignoreUnknownFields = false)
-@PropertySource("classpath:config.properties")
+@ConfigurationProperties(prefix = "dbswitch", ignoreInvalidFields = false, ignoreUnknownFields = false)
+@PropertySource(
+        value = {"classpath:config.properties", "classpath:config.yml"},
+        ignoreResourceNotFound=true,
+        factory = DbswitchPropertySourceFactory.class)
 public class DbswichProperties {
 
-	private List<SourceDataSourceProperties> source = new ArrayList<>();
+    private List<SourceDataSourceProperties> source = new ArrayList<>();
 
-	private TargetDataSourceProperties target = new TargetDataSourceProperties();
+    private TargetDataSourceProperties target = new TargetDataSourceProperties();
 
-	@Data
-	@NoArgsConstructor
-	public static class SourceDataSourceProperties {
-		private String url;
-		private String driverClassName;
-		private String username;
-		private String password;
+    @Data
+    public static class SourceDataSourceProperties {
+        private String url;
+        private String driverClassName;
+        private String username;
+        private String password;
 
-		private Integer fetchSize=5000;
-		private String sourceSchema="";
-		private String prefixTable="";
-		private String sourceIncludes="";
-		private String sourceExcludes="";
-	}
+        private Integer fetchSize = 5000;
+        private String sourceSchema = "";
+        private String prefixTable = "";
+        private String sourceIncludes = "";
+        private String sourceExcludes = "";
+    }
 
-	@Data
-	@NoArgsConstructor
-	public static class TargetDataSourceProperties {
-		private String url;
-		private String driverClassName;
-		private String username;
-		private String password;
+    @Data
+    public static class TargetDataSourceProperties {
+        private String url;
+        private String driverClassName;
+        private String username;
+        private String password;
 
-		private String targetSchema="";
-		private Boolean targetDrop=Boolean.TRUE;
-		private Boolean createTableAutoIncrement=Boolean.FALSE;
-		private Boolean writerEngineInsert=Boolean.FALSE;
-		private Boolean changeDataSynch=Boolean.FALSE;
-	}
+        private String targetSchema = "";
+        private Boolean targetDrop = Boolean.TRUE;
+        private Boolean createTableAutoIncrement = Boolean.FALSE;
+        private Boolean writerEngineInsert = Boolean.FALSE;
+        private Boolean changeDataSynch = Boolean.FALSE;
+    }
 }
